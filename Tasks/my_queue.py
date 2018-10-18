7e3e444b415a333a24401d98dd1045cd55fe7c87
dvnesterov <denis.v.nesterov@gmail.com>
2018-10-18 20:56:23 +0300

    v18-10-18

--- Tasks/my_queue.py
+++ Tasks/my_queue.py
@@ -2,55 +2,61 @@
 My little Queue
 """
 
+
 queue = []
 
-def enqueue(elem) -> None:
-	"""
-	Operation that add element to the end of the queue
 
-	:param elem: element to be added
-	:return: Nothing
-	"""
-	global queue
-	queue[:0] = [elem]
-	return None
+def enqueue(elem) -> None:
+    """
+    Operation that add element to the end of the queue
+
+    :param elem: element to be added
+    :return: Nothing
+    """
+
+    global queue
+    queue[:0] = [elem]
+    return None
 
 
 def dequeue():
-	"""
-	Return element from the beginning of the queue
+    """
+    Return element from the beginning of the queue
 
-	:return: dequeued element
-	"""
-	global queue
+    :return: dequeued element
+    """
 
-	if len(queue) == 0:
-		return None
 
-	res = queue[len(queue) - 1]
-	queue = queue[:-1]
 
-	return res
+    global queue
+
+    if len(queue) == 0:
+        return None
+
+    res = queue[len(queue) - 1]
+    queue = queue[:-1]
+
+    return res
 
 
 def peek(ind: int = 0):
-	"""
-	Allow you to see at the element in the queue without dequeuing it
+    """
+    Allow you to see at the element in the queue without dequeuing it
 
-	:param ind: index of element (count from the beginning)
-	:return: peeked element
-	"""
-	ret = queue[len(queue) - 1 - ind]
-	return ret
+    :param ind: index of element (count from the beginning)
+    :return: peeked element
+    """
+    ret = queue[len(queue) - 1 - ind]
+    return ret
 
 
 def clear() -> None:
-	"""
-	Clear my queue
+    """
+    Clear my queue
 
-	:return: None
-	"""
-	global queue
-	queue = []
+    :return: None
+    """
+    global queue
+    queue = []
 
-	return None
+    return None
--- Tasks/priority_queue.py
+++ Tasks/priority_queue.py
@@ -5,39 +5,56 @@ Queue priorities are from 0 to 5
 """
 
 
-def enqueue(elem, priority: int = 0) -> None:
-	"""
-	Operation that add element to the end of the queue
+queue = [][]
 
-	:param elem: element to be added
-	:return: Nothing
-	"""
-	return None
+
+def enqueue(elem, priority: int = 0) -> None:
+    """
+    Operation that add element to the end of the queue
+
+    :param elem: element to be added
+    :return: Nothing
+    """
+
+    global queue
+
+    queue[priority][:0] = [elem]
+
+    return None
 
 
 def dequeue():
-	"""
-	Return element from the beginning of the queue
+    """
+    Return element from the beginning of the queue
 
-	:return: dequeued element
-	"""
-	return None
+    :return: dequeued element
+    """
+
+    global queue
+
+    if len(queue) == 0:
+        return None
+
+    res = queue[len(queue) - 1]
+    queue = queue[:-1]
+
+    return None
 
 
 def peek(ind: int = 0, priority: int = 0):
-	"""
-	Allow you to see at the element in the queue without dequeuing it
+    """
+    Allow you to see at the element in the queue without dequeuing it
 
-	:param ind: index of element (count from the beginning)
-	:return: peeked element
-	"""
-	return None
+    :param ind: index of element (count from the beginning)
+    :return: peeked element
+    """
+    return None
 
 
 def clear() -> None:
-	"""
-	Clear my queue
+    """
+    Clear my queue
 
-	:return: None
-	"""
-	return None
+    :return: None
+    """
+    return None
