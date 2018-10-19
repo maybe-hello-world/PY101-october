66b9a4445cb8a10e2355df76f61fdb95422a7a9b
gitschon <schon@inbox.ru>
2018-10-19 20:12:17 +0300

    hw pri eqe done

--- Tasks/priority_queue.py
+++ Tasks/priority_queue.py
@@ -4,6 +4,7 @@ Priority Queue
 Queue priorities are from 0 to 5
 """
 
+my_pri_qu = [[] for i in range(6)]
 
 def enqueue(elem, priority: int = 0) -> None:
 	"""
@@ -12,6 +13,19 @@ def enqueue(elem, priority: int = 0) -> None:
 	:param elem: element to be added
 	:return: Nothing
 	"""
+	if priority == 0:
+		my_pri_qu[0].append(elem)
+	elif priority == 1:
+		my_pri_qu[1].append(elem)
+	elif priority == 2:
+		my_pri_qu[2].append(elem)
+	elif priority == 3:
+		my_pri_qu[3].append(elem)
+	elif priority == 4:
+		my_pri_qu[4].append(elem)
+	elif priority == 5:
+		my_pri_qu[5].append(elem)
+
 	return None
 
 
@@ -21,7 +35,14 @@ def dequeue():
 
 	:return: dequeued element
 	"""
-	return None
+	returned = None
+	for i in range(len(my_pri_qu)):
+		if len(my_pri_qu[i]) == 0:
+			continue
+		returned = my_pri_qu[i].pop(0)
+		break
+
+	return returned
 
 
 def peek(ind: int = 0, priority: int = 0):
@@ -31,13 +52,18 @@ def peek(ind: int = 0, priority: int = 0):
 	:param ind: index of element (count from the beginning)
 	:return: peeked element
 	"""
-	return None
+	if len(my_pri_qu[priority]) == 0:
+		return print("My_stack is empty")
+	return my_pri_qu[priority][ind]
 
 
 def clear() -> None:
-	"""
-	Clear my queue
+    """
+    Clear my queue
 
-	:return: None
-	"""
-	return None
+    :return: None
+    """
+    for i in my_pri_qu:
+        i.clear()
+
+    return None
