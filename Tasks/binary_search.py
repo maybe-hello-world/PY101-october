86ff6ac9c086777e3cb5899664817a7375e55339
gitschon <schon@inbox.ru>
2018-10-21 23:59:09 +0300

    my second hw bigins

--- Tasks/binary_search.py
+++ Tasks/binary_search.py
@@ -1,3 +1,8 @@
+import numpy as np
+arr = np.array([i + 1 for i in range(1, 10)])
+print(arr)
+
+
 def binary_search(elem, arr):
 	"""
 	Performs binary search of given element inside of array
@@ -6,5 +11,16 @@ def binary_search(elem, arr):
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
+	left = 0
+	right = len(arr)
+	half = 0
 
-	return None
+	while arr[half] != elem:
+		half = (right + left) // 2
+		if arr[half] < elem:
+			left = half
+		else:
+			right = half
+	return half
+
+print(binary_search(5,arr))
