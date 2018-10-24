eb39326413f3dabaf312f22103544643d09308be
dvnesterov <denis.v.nesterov@gmail.com>
2018-10-24 17:05:35 +0300

    Homework 2018-10-19 (w/priority queue)

--- Tasks/binary_search.py
+++ Tasks/binary_search.py
@@ -7,4 +7,17 @@ def binary_search(elem, arr):
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
 
+	mid = len(arr) // 2
+	print(mid)
+
+	while arr(mid) != elem:
+		pass
+
+
 	return None
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+el = 3
+
+binary_search(el, arr)
