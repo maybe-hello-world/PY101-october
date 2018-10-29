18390017bd5887b4de275c7f6bc07adf285b5bb5
unknown <isa@TO-IP235.v6.vosstaniya6.inner>
2018-10-29 17:16:32 +0300

    3

--- Tasks/binary_search.py
+++ Tasks/binary_search.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 
 # arr = np.array([i for i in range(10)])
@@ -32,34 +31,3 @@ def binary_search(elem, arr, l=0, r=-1):
             return binary_search(elem, arr, l = mid + 1, r=r)
         else:
             return binary_search(elem, arr, l=l, r = mid - 1)
-=======
-import numpy as np
-
-# arr = np.array([i for i in range(10)])
-# arr = np.array([])
-
-def binary_iter(elem, arr):
-    l = 0
-    r = len(arr)
-    mid = (l + r) // 2
-    while arr[mid] != elem:
-        if arr[mid] < elem:
-            l = mid + 1
-        else:
-            r = mid - 1
-        mid = (l + r) // 2
-    return print(mid)
-
-def binary_search(elem, arr, l=0, r=-1):
-    if r == -1:
-        r = len(arr)
-    if l > r or elem < 0:return None
-    # if elem not in arr: #decrease from O(logn) to O(nlogn), bad idea use it
-    #     return None
-    mid = (l + r) // 2
-    if mid == elem: return mid
-    if arr[mid] < elem:
-        return binary_search(elem, arr, l=mid + 1, r=r)
-    else:
-        return binary_search(elem, arr, l=l, r=mid - 1)
->>>>>>> c8e155868851cabed95a1967c3a8b89d4c5b1b45
