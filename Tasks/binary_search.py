3169d3843d01fa00f43717e81c33f71208a5fc5c
zinchenkovasil <zinchenkovasil@mail.ru>
2018-10-30 13:39:34 +0300

    binary_search (recurs)

--- Tasks/binary_search.py
+++ Tasks/binary_search.py
@@ -1,46 +1,33 @@
-def binary_search(elem, arr):
-<<<<<<< HEAD
-    local_mid = len(arr) // 2
-    if elem == arr[local_mid]:
-        print("элемент найден по индексу: ", local_mid)
-        return local_mid
-    elif len(arr) == 1:
+#необходимо передавать левую и правую границы
+
+def binary_search(elem, arr, left = 0, right = -1):
+    """
+    Performs binary search of given element inside of array
+    :param elem: element to be found
+    :param arr: array where element is to be found
+    :return: Index of element if it's presented in the arr, None otherwise
+    """
+    if right == -1:
+        right = len(arr)
+    if elem < arr[0] or elem > arr[len(arr) - 1]:
         return None
-    elif elem > arr[local_mid]:
-        arr1 = arr[local_mid:]
-        return search(arr1, elem, local_mid, False)
-    elif elem < arr[local_mid]:
-        arr2 = arr[:local_mid]
-        return search(arr2, elem, local_mid, True)
-
-
-
-
-def search(arr, elem, global_mid, left):
-    local_mid = len(arr) // 2
-    if left:
-        global_mid = global_mid - len(arr) + local_mid
+    if left > right:
+        return None
     else:
-        global_mid = global_mid + local_mid
-    if elem == arr[local_mid]:
-        print("элемент найден по индексу: ", global_mid)
-        return global_mid
-    elif len(arr) == 1:
-        return None
-    elif elem > arr[local_mid]:
-        arr1 = arr[local_mid:]
-        return search(arr1, elem, global_mid, False)
-    elif elem < arr[local_mid]:
-        arr2 = arr[:local_mid]
-        return search(arr2, elem, global_mid, True)
-=======
-	"""
-	Performs binary search of given element inside of array
+        mid = (right + left) // 2
+        if arr[mid] == elem:
+            return mid
+        elif elem < arr[mid]:
+            return binary_search(elem, arr, left, mid - 1)
+        else:
+            return binary_search(elem, arr, mid + 1, right)
 
-	:param elem: element to be found
-	:param arr: array where element is to be found
-	:return: Index of element if it's presented in the arr, None otherwise
-	"""
 
-	return None
->>>>>>> 0a10cd6c85aa9486d3350e979e6bd177e77e78e6
+
+arr = [i for i in range(100)] + [101]
+print(binary_search(55, arr))
+
+
+
+
+
