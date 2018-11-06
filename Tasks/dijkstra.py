bae8a76347af937414a9dd160ffcb2c7baac0be8
unknown <isa@TO-IP235.v6.vosstaniya6.inner>
2018-11-06 16:30:22 +0300

    dijkstra

--- Tasks/dijkstra.py
+++ Tasks/dijkstra.py
@@ -9,5 +9,24 @@ def dijkstra_algo(G, starting_node) -> dict:
 	:param starting_node: starting node from G
 	:return: dict like {'node1': 0, 'node2': 10, '3': 33, ...}, where nodes are nodes from G
 	"""
+	visited = []
+	stoimost = {i: float("inf") for i in list(G.nodes)}
+	stoimost[starting_node] = 0
+	while len(visited) != len(G.nodes):
+		def min_n():
+			key = ""
+			value = float('inf')
+			for j in stoimost:
+				if j not in visited and stoimost[j] <= value:
+						key = j
+						value = stoimost[j]
+			return key
 
-	return dict()
+		miv = min_n()
+		for i in G.neighbors(miv):
+			if stoimost[i] > stoimost[miv] + G[miv][i]['weight']:
+				stoimost[i] = stoimost[miv] + G[miv][i]['weight']
+		visited.append(miv)
+	return stoimost
+
+	# return dict()
