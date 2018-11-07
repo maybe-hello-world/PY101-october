f1f1c257c077c87c405bce7063bff4fc6de12dcd
Victor Foteev <vfoteev@v6.vosstaniya6.inner>
2018-11-07 17:35:19 +0300

    lineral search is added

--- Tasks/linear_search.py
+++ Tasks/linear_search.py
@@ -2,9 +2,30 @@
 This module implements some functions based on linear search algo
 """
 import networkx as nx
-import Tasks.my_queue as que
 
 
+# G = nx.Graph()
+# nodes = list("ABCDEFGH")
+# G.add_nodes_from(nodes)
+#
+# edges = [
+#     ('A', 'B', 1),
+#     ('A', 'C', 17),
+#     ('A', 'D', -3),
+#     ('A', 'E', 14),
+#     ('A', 'F', 2),
+#     ('B', 'C', 99),
+#     ('B', 'E', 132),
+#     ('C', 'D', 21),
+#     ('C', 'F', 331),
+#     ('E', 'D', 14),
+#     ('E', 'F', -36),
+#     ('G', 'F', -36),
+#     ('H', 'F', -36),
+#     ('G', 'H', -35.9999999)
+# ]
+#
+# G.add_weighted_edges_from(edges)
 
 def min_search(arr) -> int:
     """
@@ -22,16 +43,26 @@ def min_search(arr) -> int:
     return my_min
 
 
-def min_weight_search(Graph) -> tuple:
-	"""
-	Function that find edge in graph with minimal weight of all
+def min_weight_search(Graph):
+    """
+    Function that find edge in graph with minimal weight of all
 
-	:param Graph: NetworkX Graph (or digraph) with weighted edges
-	:return: tuple of nodes (node, node) the weight of edge between which is minimal (any occurrence)
-	"""
+    :param Graph: NetworkX Graph (or digraph) with weighted edges
+    :return: tuple of nodes (node, node) the weight of edge between which is minimal (any occurrence)
+    """
+    result = []
+    my_min = Graph.get_edge_data(*list(Graph.edges)[0])['weight']
+    for gr in Graph.edges:
+        my_cost = int(Graph.get_edge_data(*gr)['weight'])
+        if my_cost < my_min:
+            my_min = my_cost
+    #print(my_min)
+    for gr in Graph.edges:
+        my_cost = int(Graph.get_edge_data(*gr)['weight'])
+        if my_min == my_cost:
+            result.append(gr)
+
+    return result
 
 
-
-
-	return None, None
-
+#print(min_weight_search(G))
