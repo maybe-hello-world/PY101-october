a69946b680a306045717ae5a047f6874e4f51d7d
unknown <skykatty@gmail.com>
2018-11-07 22:03:12 +0300

    add stack

--- Tasks/my_stack.py
+++ Tasks/my_stack.py
@@ -2,8 +2,9 @@
 My little Stack
 """
 
-
+my_stack = []
 def push(elem) -> None:
+	my_stack.append(elem)
 	"""
 	Operation that add element to stack
 
@@ -14,25 +15,34 @@ def push(elem) -> None:
 
 
 def pop():
+	if my_stack == []:
+		return None
+	else:
+		elem = my_stack.pop()
 	"""
 	Pop element from the top of the stack
 
 	:return: popped element
 	"""
-	return None
+	return elem
 
 
 def peek(ind: int = 0):
+	if my_stack == []:
+		return None
+	else:
+		peek_elem = my_stack[-1-ind]
 	"""
 	Allow you to see at the element in the stack without popping it
 
 	:param ind: index of element (count from the top)
 	:return: peeked element
 	"""
-	return None
+	return peek_elem
 
 
 def clear() -> None:
+	my_stack.clear()
 	"""
 	Clear my stack
 
