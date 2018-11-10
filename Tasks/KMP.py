84a22a397290ae56075d99473dc77a3ff672f0ef
dvnesterov <43860456+dvnesterov@users.noreply.github.com>
2018-11-10 12:01:20 +0300

    KMP added (#26)
    * Homework 2018-10-19 (w/priority queue)
    
    * dijkstra task added
    
    * Homework 2018-10-19 (w/priority queue)
    
    * DJI
    
    * KMP and linear_search tasks added
    
    * bugfix
    
    * v2
    
    * v18-10-18
    
    * v18-10-18
    
    * Homework 2018-10-19 (w/priority queue)
    
    * Homework 2018-10-19 (w/priority queue)
    
    * Homework 2018-10-19 (w/priority queue)
    
    * Homework 2018-10-19 (w/priority queue)
    
    * Homework 2018-10-19 (w/priority queue)
    
    * DJI
    
    * Dijkstra: версия по шаблону
    
    * Dijkstra: версия по шаблону
    
    * Dijkstra: версия по шаблону
    
    * Dijkstra: версия по шаблону

--- Tasks/KMP.py
+++ Tasks/KMP.py
@@ -15,6 +15,74 @@ def KMP_algo(inp_string: str, substr: str):
 		:return: prefix values table
 		"""
 
-		return []
+
+		f = []
+
+		for i in range(len(prefix_str)):
+			j = i
+			offset = 0
+			while j > 0:
+				j -= 1
+				# print(i, j, offset, prefix_str[j], prefix_str[i-offset])
+				if prefix_str[j] == prefix_str[i - offset]:
+					offset += 1
+				else:
+					j += offset
+					offset = 0
+			f.append(offset)
+			# print('append', offset)
+
+		return f
+
+	'''
+		Важный Комментарий: первоначально был сделан вариант, который ищет не только первое, но и 
+		последующие вхождения подстроки в строку и много тестовых выводов. Не удалял. 
+		Поэтому в коде есть костыли и некоторая непричёсанность =) 		
+	'''
+
+
+	f = prefix_fun(substr)
+	# print(f)
+
+	i = 0
+	j = 0
+	done = False
+
+	while not done:  # (input("Exit - 0") != '0')
+		initf = False
+		done = False
+
+		a = inp_string[i:i + 1]
+		b = substr[j:j + 1]
+		# print('a=', a, 'b=', b)
+
+		if a == b:
+			i += 1
+			j += 1
+		elif j <= 0:
+			i += 1
+		else:
+			j = f[j - 1]
+		# print('i=', i, 'j=', j)
+		# print(inp_string[:i])
+		# print(substr[:j])
+
+		if len(inp_string) == i:
+			# print("Конец поиска : конец строки")
+			done = True
+
+		if len(substr) == j:
+			# print("Подстрока найдена!", i - j)
+			initf = True
+
+			return i - j  # если закомментировать этот return, то будет искать последующие вхождеия
+
+		if initf:
+			i += 1
+			j = 0
 
 	return None
+
+
+# res = KMP_algo('ATTATGGATGATCXATGATC', 'ATGATCZ')
+# print(res)
