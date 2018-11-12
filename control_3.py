0d3d50482a5ecb24f6347e4d3d8311af1392f504
Victor Foteev <vfoteev@v6.vosstaniya6.inner>
2018-11-12 15:21:06 +0300

    new contr3 done

--- control_3.py
+++ control_3.py
@@ -1,29 +1,30 @@
 text = ['ATTA',
-        'BCTA',
-        'BGCA',
-        'BCAA']
+        'АCTA',
+        'АGCA',
+        'ACAA']
 
 def common_letter(sl:list):
     """
     :param sl: list stolb
     :return:com:char
     """
+    com = None
     di = {}
     for i in sl:
-        ke = i
-        if ke in di.keys():
-            di[ke] += 1
+        if i in di.keys():
+            di[i] += 1
         else:
-            di[ke] = 1
+            di[i] = 1
+
     m = max(di.values())
 
     for i, j in di.items():
         if j == m:
-            return i
+            com = i
 
-    return None
+    return com
 
-def get_pos_str(test:list):
+def get_pos_str(text:list):
     res =[]
     for i in range(0,len(text)):
         st = [x[i] for x in text]
