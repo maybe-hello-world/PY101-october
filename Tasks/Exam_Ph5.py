5df5688f202fc73e3edf100d65a0001b3d3482a1
unknown <isa@TO-IP235.v6.vosstaniya6.inner>
2018-11-19 17:43:24 +0300

    Exam3,5,6,7

--- Tasks/Exam_Ph5.py
+++ Tasks/Exam_Ph5.py
@@ -1,9 +1,14 @@
-arr = ['ATTA','ACTA','AGCA','ACAA']
+from collections import Counter
+# arr = ['ATTA','ACTA','AGCA','ACAA']
+arr = ['AGTA','ACTA','AGCA','ACAA']
 n = len(arr[0])
+counter = 0
 
 def cs():
     key = {}
+    invertkey = {}
     consensus = []
+    cnt=1
     for i in range(n):
         for j in arr:
             stlb = j[i]
@@ -13,10 +18,19 @@ def cs():
             else:
                 key[stlb]=1
         maxim = max(key.values())
+        # for i,j in key.items():
+        #     if j==maxim:
+        #         consensus.append(i)
         for i,j in key.items():
-            if j==maxim:
-                consensus.append(i)
+            if j in invertkey:
+                invertkey[j].join(i)
+            else:
+                invertkey[j] = i
+                if j==maxim:
+                    consensus.append(i)
+            print(invertkey)
         key.clear()
+        invertkey.clear()
     cons = ''.join(consensus)
     return cons
 # print(key)
--- Tasks/Exam_Ph7.py
+++ Tasks/Exam_Ph7.py
@@ -1,18 +1,27 @@
 import random as rnd
 
-n = 10**2
-arr = [rnd.randint(1,6) for i in range(n)]
+n = 10**6
+arr = [rnd.randint(13,25) for i in range(n)]
 print(arr)
 
 def sort(arr,left,right):
     length = (right-left)+1
     s = [0 for _ in range(length)]
+    # print(s)
     st = []
     for i in arr:
-        s[i] += 1
+        s[i-length] += 1
+    # print(s)
     for i in range(length):
         for j in range(s[i]):
-            st.append(i)
+            st.append(i+length)
     return st
 
 print(sort(arr,13,25))
+
+# x = sort(arr,13,25)
+# x = [rnd.randint(13,25) for i in range(n)]
+# for i in x:
+#     if x[i] > x[i+1]:
+#         print("Array not sort")
+#         break
